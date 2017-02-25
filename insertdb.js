4f7633e337ae39f613fc17a8d26f5ef370b57a14
Mitchell Etzel <metzel@ucsc.edu>
2017-02-25 14:13:37 -0800

    Removed popup.js

--- insertdb.js
+++ insertdb.js
@@ -1,4 +1,36 @@
 $(function (){
+
+    //Format login data
+    var $username = $('#username');
+    var $password = $('#password');
+    $('#login').on('click', function() {
+        var login_info = "user=" + $username.val() + "&" + "pass=" + $password.val();
+
+        console.log(login_info);
+
+        //POST - Send login data to server-side script for processing
+        $.post('http://Sample-env.zssmubuwik.us-west-1.elasticbeanstalk.com/login.php', login_info, function (message) {
+            console.log(message);
+            /*if(message == 1){
+                /!*successPage();*!/
+            }*/
+        });
+    });
+    //Clear form elements when pressing 'clear'
+    $('#clear').click(function () {
+        $('.login-prompt')[0].reset();
+    });
+});
+
+function successPage(){
+    /*chrome.browserAction.setPopup({
+        popup:"success.html"
+    });*/
+}
+
+
+
+$(function (){
 	$('#SendData').on('click', function() {
 		genURLData(); 
 	});
